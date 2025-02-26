99e11c2240e3166ed72b6c6353194aeacd66c96a
Gustavo Rodrigues <guhrodriguesads@gmail.com>
2025-02-25 21:17:50 -0300

    Ajustes necessários

--- dw_adventureworks/models/marts/sales/fat_vendas.sql
+++ dw_adventureworks/models/marts/sales/fat_vendas.sql
@@ -24,30 +24,25 @@ dim_pessoa as (
 
 ),
 
-dim_tempo as (
-
-    select * from {{ ref('dim_tempo') }}
-
-),
-
 fat_vendas as (
 
-        select
-            dim_solic_venda.sk_venda
-            ,dim_solic_venda.fk_cartao
-            ,dim_solic_venda.fk_endereco
-            ,dim_solic_venda.fk_produto
-            ,dim_solic_venda.fk_motivo_venda
-            ,{{ dbt_utils.generate_surrogate_key(['dim_solic_venda.sk_venda','dim_solic_venda.dat_venda']) }} as fk_tempo
-            ,dim_pessoa.id_pessoa
-            ,dim_solic_venda.qtd_ordem_venda
-            ,dim_solic_venda.preco_unitario
-            ,dim_solic_venda.desconto_preco_unitario
-            ,dim_solic_venda.subtotal
-        from dim_solic_venda
-        left join dim_cartao on dim_solic_venda.fk_cartao = dim_cartao.id_cartao
-        left join dim_pessoa on dim_cartao.id_pessoa = dim_pessoa.id_pessoa
-        left join dim_endereco on dim_solic_venda.fk_endereco = dim_endereco.id_endereco
-    )
+    select
+        dim_solic_venda.sk_venda
+        ,dim_solic_venda.fk_cartao
+        ,dim_solic_venda.fk_endereco
+        ,dim_solic_venda.fk_produto
+        ,dim_solic_venda.fk_motivo_venda
+        ,dim_pessoa.id_pessoa
+        ,dim_solic_venda.dat_venda
+        ,dim_solic_venda.qtd_ordem_venda
+        ,dim_solic_venda.preco_unitario
+        ,dim_solic_venda.desconto_preco_unitario
+        ,dim_solic_venda.subtotal
+    from dim_solic_venda
+    left join dim_cartao on dim_solic_venda.fk_cartao = dim_cartao.id_cartao
+    left join dim_pessoa on dim_cartao.id_pessoa = dim_pessoa.id_pessoa
+    left join dim_endereco on dim_solic_venda.fk_endereco = dim_endereco.id_endereco
+
+)
  
 select * from fat_vendas
